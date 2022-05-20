0b2af457c034dc6820f2f00a49de5214f3b05cae
layconjohn <layconjohn@gmail.com>
2022-05-19 22:12:34 -0300

    Pegar o objeto da API dos filmes

--- projeto9-cineflex/src/components/filmes/Filmes.js
+++ projeto9-cineflex/src/components/filmes/Filmes.js
@@ -1,81 +1,9 @@
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import "./estilos.css"
-
-const filmes = [
-	{
-        id: 1,
-        title: "2067",
-        posterURL: "https://m.media-amazon.com/images/M/MV5BZDJmODA5NGEtZmU1ZC00NDRkLWJkYmEtYmUxYzY0NmY4MTYwXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_QL75_UY562_CR7,0,380,562_.jpg",
-        overview: "A lowly utility worker is called to the future by a mysterious radio signal, he must leave his dying wife to embark on a journey that will force him to face his deepest fears in an attempt to change the fabric of reality and save humankind from its greatest environmental crisis yet.",
-        releaseDate: "2020-10-01T00:00:00.000Z",
-    },
-    {
-        id: 2,
-        title: "Welcome to Sudden Death",
-        posterURL: "https://image.tmdb.org/t/p/w500/elZ6JCzSEvFOq4gNjNeZsnRFsvj.jpg",
-        overview: "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
-        releaseDate: "2020-09-29T00:00:00.000Z",
-    },
-
-    {
-        id: 3,
-        title: "Welcome to Sudden Death",
-        posterURL: "https://image.tmdb.org/t/p/w500/elZ6JCzSEvFOq4gNjNeZsnRFsvj.jpg",
-        overview: "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
-        releaseDate: "2020-09-29T00:00:00.000Z",
-    },
-    {
-        id: 4,
-        title: "Welcome to Sudden Death",
-        posterURL: "https://image.tmdb.org/t/p/w500/elZ6JCzSEvFOq4gNjNeZsnRFsvj.jpg",
-        overview: "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
-        releaseDate: "2020-09-29T00:00:00.000Z",
-    },
-    {
-        id: 5,
-        title: "Welcome to Sudden Death",
-        posterURL: "https://image.tmdb.org/t/p/w500/elZ6JCzSEvFOq4gNjNeZsnRFsvj.jpg",
-        overview: "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
-        releaseDate: "2020-09-29T00:00:00.000Z",
-    },
-    {
-        id: 6,
-        title: "Welcome to Sudden Death",
-        posterURL: "https://image.tmdb.org/t/p/w500/elZ6JCzSEvFOq4gNjNeZsnRFsvj.jpg",
-        overview: "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
-        releaseDate: "2020-09-29T00:00:00.000Z",
-    },
-    {
-        id: 7,
-        title: "Welcome to Sudden Death",
-        posterURL: "https://image.tmdb.org/t/p/w500/elZ6JCzSEvFOq4gNjNeZsnRFsvj.jpg",
-        overview: "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
-        releaseDate: "2020-09-29T00:00:00.000Z",
-    },
-    {
-        id: 8,
-        title: "Welcome to Sudden Death",
-        posterURL: "https://image.tmdb.org/t/p/w500/elZ6JCzSEvFOq4gNjNeZsnRFsvj.jpg",
-        overview: "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
-        releaseDate: "2020-09-29T00:00:00.000Z",
-    },
-    {
-        id: 9,
-        title: "Welcome to Sudden Death",
-        posterURL: "https://image.tmdb.org/t/p/w500/elZ6JCzSEvFOq4gNjNeZsnRFsvj.jpg",
-        overview: "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
-        releaseDate: "2020-09-29T00:00:00.000Z",
-    },
-    {
-        id: 10,
-        title: "Welcome to Sudden Death",
-        posterURL: "https://image.tmdb.org/t/p/w500/elZ6JCzSEvFOq4gNjNeZsnRFsvj.jpg",
-        overview: "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
-        releaseDate: "2020-09-29T00:00:00.000Z"
-    }
-]
 
 function Filme({valor}){
     return (
@@ -89,6 +17,20 @@ function Filme({valor}){
 }
 
 export default function Filmes() {
+    //Estado
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        const promisse = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+
+        promisse.then(response => {
+            setFilmes(response.data)
+            console.log(filmes)
+        })
+    },[])
+    
+
+
     return (
         <>
             <Titulo>Selecione o filme</Titulo>
